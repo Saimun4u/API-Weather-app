3512f2047fdac07bd3a5f99fd0f6ac815b7bb345
Saimun Hassan <saimunhas@gmail.com>
2024-05-25 21:55:09 -0400

    Added search functionality btn to enter city name and get weather data

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const apiKey = '86469fb40636fb08b5b59eb9cce4bb69';
 const apiUrl =
-  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=toronto';
-const inputEl = document.getElementById('input');
+  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
+
   console.log(data);
+
+  // Show data from API to web page
+
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+  document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+  document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
 }
 
-checkWeather();
+searchBtn.addEventListener('click', () => {
+  checkWeather(searchBox.value);
+});
 
 // inputEl.addEventListener('keyup', (e) => {
 //   if (e.target.value && e.key === 'Enter') fetchAPI(e.target.value);
