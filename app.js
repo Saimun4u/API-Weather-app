a18f9e68f4b7bad6882279cecb302dc1097baae2
Saimun Hassan <saimunhas@gmail.com>
2024-05-25 22:26:42 -0400

    Added display toggle for invalid city name if city name is incorrect

--- app.js
+++ app.js
@@ -8,32 +8,42 @@ const weatherIcon = document.querySelector('.weather-icon');
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  var data = await response.json();
 
-  // console.log(data);
+  // Check for invalid city names
 
-  // Show data from API to web page
+  if (response.status == 404) {
+    document.querySelector('.error').style.display = 'block';
+    document.querySelector('.weather').style.display = 'none';
+  } else {
+    var data = await response.json();
 
-  document.querySelector('.city').innerHTML = data.name;
-  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
-  document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-  document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+    // console.log(data);
 
-  if (data.weather[0].main == 'Clouds') {
-    weatherIcon.src = 'images/clouds.png';
-  } else if (data.weather[0].main == 'Clear') {
-    weatherIcon.src = 'images/clear.png';
-  } else if (data.weather[0].main == 'Rain') {
-    weatherIcon.src = 'images/rain.png';
-  } else if (data.weather[0].main == 'Drizzle') {
-    weatherIcon.src = 'images/drizzle.png';
-  } else if (data.weather[0].main == 'Mist') {
-    weatherIcon.src = 'images/mist.png';
+    // Show data from API to web page
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML =
+      Math.round(data.main.temp) + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+
+    if (data.weather[0].main == 'Clouds') {
+      weatherIcon.src = 'images/clouds.png';
+    } else if (data.weather[0].main == 'Clear') {
+      weatherIcon.src = 'images/clear.png';
+    } else if (data.weather[0].main == 'Rain') {
+      weatherIcon.src = 'images/rain.png';
+    } else if (data.weather[0].main == 'Drizzle') {
+      weatherIcon.src = 'images/drizzle.png';
+    } else if (data.weather[0].main == 'Mist') {
+      weatherIcon.src = 'images/mist.png';
+    }
+
+    // Unhide weather infogram
+
+    document.querySelector('.weather').style.display = 'block';
+    document.querySelector('.error').style.display = 'none';
   }
-
-  // Unhide weather infogram
-
-  document.querySelector('.weather').style.display = 'block';
 }
 
 searchBtn.addEventListener('click', () => {
